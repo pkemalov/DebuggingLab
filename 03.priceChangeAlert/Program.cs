c9b34f819312a39b39ca79566f0d5cc334bf23f6
pkemalov <petar.kemalov@gmail.com>
2017-02-06 20:36:07 +0200

    commited

---
+++ 03.priceChangeAlert/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class PriceChangeAlert
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double threshold = double.Parse(Console.ReadLine());
+        double lastPrice = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            double nextPrice = double.Parse(Console.ReadLine());
+            double differenceLastPrice = GetDifferenceLastPrice(lastPrice, nextPrice);
+            bool isSignificantDifference = GetDifference(differenceLastPrice, threshold);
+            
+            string message = GetFinalMessage(nextPrice, lastPrice, differenceLastPrice, isSignificantDifference);
+            Console.WriteLine(message);
+
+            lastPrice = nextPrice;
+        }
+    }
+
+    private static string GetFinalMessage(double nextPrice, double lastPrice, double differenceLastPrice, bool etherTrueOrFalse)
+    {
+        string to = "";
+        if (differenceLastPrice == 0)
+        {
+            to = string.Format("NO CHANGE: {0}", nextPrice);
+        }
+        else if (!etherTrueOrFalse)
+        {
+            to = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", lastPrice, nextPrice, differenceLastPrice);
+        }
+        else if (etherTrueOrFalse && (differenceLastPrice > 0))
+        {
+            to = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", lastPrice, nextPrice, differenceLastPrice);
+        }
+        else if (etherTrueOrFalse && (differenceLastPrice < 0))
+            to = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)", lastPrice, nextPrice, differenceLastPrice);
+        return to;
+    }
+    private static bool GetDifference(double differenceLastPrice, double threshold)
+    {
+        if (Math.Abs(differenceLastPrice) >= threshold*100)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static double GetDifferenceLastPrice(double lastPrice, double nextPrice)
+    {
+        double r = ((nextPrice - lastPrice) / lastPrice)*100;
+        return r;
+    }
+}
---
+++ DebuggingLab/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var result = GetMultipleOfEvenAndOdds(num);
+            Console.WriteLine(result);
+        }
+
+        private static long GetMultipleOfEvenAndOdds(int num)
+        {
+            var sumEvens = GetSumOfEvenDigits(num);
+            var sumOdds = GetSumOfOddDigits(num);
+            return sumEvens * sumOdds;
+        }
+
+        static int GetSumOfEvenDigits(int num)
+        {
+            int sum = 0;
+            num = Math.Abs(num);
+            while (num > 0)
+            {
+                var digit = num % 10;
+                if (digit % 2 == 0)
+                {
+                    sum = sum + digit;
+                }
+
+                num = num / 10;
+            }
+            return sum;
+        }
+
+        static int GetSumOfOddDigits(int num)
+        {
+            int sum = 0;
+            num = Math.Abs(num);
+            while (num > 0)
+            {
+                var digit = num % 10;
+                if (digit % 2 != 0)
+                {
+                    sum = sum + digit;
+                }
+                
+                num = num / 10;
+            }
+            return sum;
+        }
+    }
+}
